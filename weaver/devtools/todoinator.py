8c7047bfa13b9bde2d4a9189b021e7ee76f0348a
Paul Brian lifeisstillgood <paul@mikadosoftware.com>
2017-06-13 08:18:50 +0100

    add priority sorting

--- weaver/devtools/todoinator.py
+++ weaver/devtools/todoinator.py
@@ -46,10 +46,33 @@ walk a source tree, and build a dict of file: {markers...}
 return that
 
 
-#TODO: build a test framework / runner
-#TODO: build a lifecycle todo listing
+
+Todos 
+-----
+
+We have a slight finesse on the TODO.  Every TODO is assigned a
+priority. By default that is 30/100.  100 being the most hair on fire
+thing we can imagine, and 1 being lets get around to this before the 
+heat death of the universe.
+
+but we can alter that priority by putting a {val} after the text
+(we do not want to confuse with [x] form of done / not done.)
+
+
+
+Future enhancements:
+
+* "TODO-feature: "
+* have a store of todos in .todoinator in the current repo, 
+  which lets us compute what has changed ?
+
+
+
+#TODO: build a test framework / runner {20}
+#TODO: build a lifecycle todo listing {99}
 #TODO: build a ticketing system??? at least a list of todos
 #TODO: linting etc - a pre-commit check system ala phabricator
+
 
 '''
 
@@ -58,6 +81,22 @@ return that
 # TODO: build basic walk and parse and report features
 
 import os
+import re
+
+### config
+confd = {'todoinator.priorityregex': "\{\d+\}"} 
+
+class TODO(object):
+    """
+    """
+    def __init__(self, line):
+        """
+        """
+        parsedline, priority = parse_line(line)
+        self.line = parsedline
+        self.priority = priority
+        
+
 
 def keep_file(filepath):
     """Decide if we keep the filepath, solely by exlcusion of end of path
@@ -97,6 +136,7 @@ def parse_file(txt):
 
     >>> parse_file("# todo: foo\\n foo")
     [' foo']
+    
     """
     res = []
     for line in txt.split('\n'):
@@ -106,17 +146,39 @@ def parse_file(txt):
                 stpoint = line.lower().find(token)
                 if stpoint >-1:
                     res.append(line[stpoint+len(token):])
+    
     return res
+
+def parse_line(todoline):
+    """extract data from a todo line
+    
+    >>> parse_line(" some note unadorned")
+    (' some note unadorned', 30)
+    >>> parse_line(" some note {88}")
+    (' some note ', 88)
+    
+    """
+    rgx = re.compile(confd['todoinator.priorityregex'])
+    vals = rgx.findall(todoline) # returns [] or ['{4}']    
+    if vals:
+        token = sorted(vals)[-1]
+        priority = int(token.replace("{", "").replace("}", ""))
+    else:
+        token = ''
+        priority = 30
+    return todoline.replace(token, ''), priority
+
 
 def parse_tree(rootpath):
     """
     """
     for filepath in walk_tree(rootpath):
         todo_list = parse_file(open(filepath).read())
-        if todo_list:
+        res = reversed(sorted([TODO(line) for line in todo_list], key=lambda t: t.priority))        
+        if res:
             print "\n" + filepath
-            for todoline in todo_list:
-                print "-", todoline
+            for todoobj in res:
+                print "-", todoobj.line, todoobj.priority
 
 if __name__ == '__main__':
     import doctest
